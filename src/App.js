8e6ba443696741e1b459f737af2e9e58208126ef
AnimateReality <ronaldchittinappilly@gmail.com>
2022-11-25 11:02:43 +1300

    Removed elements
    Removed elements to simplify page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 
 import "./App.css";
 import Profile from "./components/Profile";
-import logo from "./images/logo.svg";
-import IFG from "./images/IFG.png";
-import Slack from "./images/slack.svg";
 import github from "./images/github.svg";
 
 
@@ -11,37 +8,22 @@ function App() {
   return (
     <div className="App">
       <Profile />
+      
       <section className="links">
-        <a target="_blank" id="twitter" href="https://twitter.com/AnimateReaIity" rel="noreferrer">
-          Twitter Link
-        </a>
-        <a target="_blank" id="btn__zuri" href="https://training.zuri.team/" rel="noreferrer">
-          Zuri Team
-        </a>
-        <a target="_blank" id="books" href="http://books.zuri.team/" rel="noreferrer">
-          Zuri Books
-        </a>
-        <a
+      <a
           target="_blank"
-          id="book__python"
-          href="https://books.zuri.team/python-for-beginners?ref_id=ronald_chittinapilly" rel="noreferrer"
-        >
-          Python Books
+          id="personal_wesbite"
+          href="https://ronald-chittinappilly-portfolio.netlify.app/" rel="noreferrer">
+          Personal Website
         </a>
-        <a target="_blank" id="pitch" href="https://background.zuri.team/" rel="noreferrer">
-          Background Check for Coders
+        <a target="_blank" id="github" href="https://github.com/AnimateReality" rel="noreferrer">
+          GitHub
         </a>
-        <a
-          target="_blank"
-          id="book__design"
-          href="https://books.zuri.team/design-rules" rel="noreferrer"
-        >
-          Design Books
+        <a target="_blank" id="linkedin" href="https://www.linkedin.com/in/ronald-chittinappilly" rel="noreferrer">
+          LinkedIn
         </a>
+       
         <div className="icons">
-          <a href="https://slack.com/">
-            <img src={Slack} alt="slack"/>
-          </a>
           <a href="https://github.com/AnimateReality" target="_blank" rel="noreferrer">
           <img src={github} alt="github"/>
           </a>
@@ -49,9 +31,9 @@ function App() {
       </section>
       <footer>
         <div className="content">
-          <img src={logo} alt="zuri logo" />
-          <p>HNG Internship 9 Frontend Task</p>
-          <img src={IFG} alt="I4G logo" />
+
+          Ronald LinkTree
+         
         </div>
       </footer>
     </div>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,15 @@
 import "./Profile.css";
-import { IoMdShareAlt } from 'react-icons/io';
+
 
 function Profile() {
   return (
     <section className="profile">
     <img id="profile__img" src="https://github.com/AnimateReality.png" alt="pfp" />
-      <button className="profile_btn">
-      <IoMdShareAlt/>
-      </button>
       <h2>Ronald Chittinappilly</h2>
-      <p id="twitter">@AnimateReality</p>
-      <p id="slack">Ronald Chittinappilly</p>
     </section>
   );
 }
+
+
 
 export default Profile;
